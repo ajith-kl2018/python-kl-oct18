19a7bd310358a35b783e2dbc2d3fc415332fea21
Ajith Kumar <ajith.kumar@outlook.com>
2018-10-30 15:04:45 +1100

    String

---
+++ stringcheck.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 30 15:01:23 2018
+
+@author: ajithkumar
+"""
+
+message = "Test Message"
+
+print (message.lower())
+print (message.upper())
+print (message.swapcase())
+
