bcda362c31bede7ea977b3303469b7d28d37f1ef
Ajith Kumar <ajith.kumar@outlook.com>
2018-10-30 15:12:02 +1100

    Typecast

---
+++ typecheck.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 30 15:09:24 2018
+
+@author: ajithkumar
+"""
+
+salary = input ("Please enter your salary")
+bonus = input ("Please enter your bonus")
+#payCheck = salary + bonus
+payCheck = int(salary) + int(bonus)
+print (payCheck)
