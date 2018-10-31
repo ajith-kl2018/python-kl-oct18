72c188e93af0ee9af0cbc138e26f8ba398776b28
Ajith Kumar <ajith.kumar@outlook.com>
2018-10-31 14:27:17 +1100

    Conditions

---
+++ condition.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 31 14:20:08 2018
+
+@author: ajithkumar
+"""
+
+answer = input("Would you like an Express Shipping? ")
+if answer == "yes" : 
+    print("That will be an extra 10$")
+
+print("Thank you")
