95ebb153702525dcd2fc00f6730b4f2e95030e3e
Ajith Kumar <ajith.kumar@outlook.com>
2018-11-01 14:21:25 +1100

    Loops

---
+++ loop-while.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  1 14:13:39 2018
+
+@author: ajithkumar
+"""
+
+answer = 0
+
+while answer != 4:
+    answer= int(input("What is 2+2 ? "))
+
+print("Yes! 2+2 = 4")
+
---
+++ loop.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  1 13:59:57 2018
+
+@author: ajithkumar
+"""
+
+#for steps in range(4):
+#for steps in range(1,10,2):
+#for steps in [1,2,3,4,5]:
+#for steps in ['red','green','blue','black']:
+for steps in ['red','green','blue','black',8]:
+    print (steps)
+    
+    
