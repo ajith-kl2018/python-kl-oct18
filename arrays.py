07bb6933703c7697647f6a230d1d06244c686568
Ajith Kumar <ajith.kumar@outlook.com>
2018-11-01 14:58:03 +1100

    Arrays

---
+++ arrays.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  1 14:34:44 2018
+
+@author: ajithkumar
+"""
+
+guests = ['first','second','third']
+#print(guests[2])
+#print(guests[-1])
+
+#print('First value default :' + guests[0])
+
+guests[0] = 'Steve'
+
+#print('First value is now :' + guests[0])
+
+guests.append('New Guy')
+
+#print('New value is now :' + guests[-1])
+
+#print('2nd Element is :' + guests[1])
+
+#guests.remove('second')
+#del guests[1]
+
+#print('2nd Element After remove is :' + guests[1])
+
+#check the index position
+#print(guests.index('fourth'))
+
+#for step in range(4):
+for step in range(len(guests)):
+    print(guests[step])
+    
+
+scores = [78,68,88,98,25]
+#print(scores[3])
+#print(scores[-2])
+
+
+
