b260ccd03da0fd5e1dbd0a0283a063eb9ad282bf
Ajith Kumar <ajith.kumar@outlook.com>
2018-11-09 14:59:30 +1100

    RegEx3

--- regex2.py
+++ regex2.py
@@ -9,10 +9,17 @@ for line in hand :
     #if re.search('From:',line) :
     #if re.search('^From:', line):
     #if re.search('^X.*:', line):
-    if re.search('^X-\S+:', line):
-        print(line)
+    #if re.search('^X-\S+:', line):
+    
+    #if re.search('[0-9][A-Z]+', line):
+    #    print(line)
 
     #print(re.findall('[0-9]+',line))
+
+x = 'From: Using the : character'
+#print(re.findall('^F.+:',x)) #Greedy match
+print(re.findall('^F.+?:',x)) #Non-Greedy match
+
 
 
 
--- testclass.py
+++ testclass.py
@@ -21,7 +21,7 @@ obj1.myName()
 obj2 = Test("Obj2")
 obj2.myName()
 
-#print(f"Class Name {Test.name}")
+print(f"Class Name {Test.name}")
 print(f"Obj1 Name {obj1.name}")
 print(f"Obj2 Name {obj2.name}")
 
